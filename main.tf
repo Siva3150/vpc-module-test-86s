04883d38b1a49024bb03639b24102821a8eff612
Siva prasad <sivaprasad3150@gmail.com>
2025-11-18 19:52:10 +0530

    user-profile

--- main.tf
+++ main.tf
@@ -4,11 +4,33 @@ module "vpc" {
     # project = "roboshop"
     # environment = "dev"
 
+    
+    # VPC
     source = "../terraform-aws-vpc-86s"
     vpc_cidr = var.vpc_cidr
     project = var.project
     environment = var.environment
     vpc_tags = var.vpc_tags
+    
+
+    # public subnets
+    public_subnet_cidrs = var.public_subnet_cidrs
+    
+
+    # private subnets
+    private_subnet_cidrs = var.private_subnet_cidrs
+    
+
+    #database subnets
+    database_subnet_cidrs = var.database_subnet_cidrs
+
+    is_peering_required = true 
 
 
 }
+
+# data "aws_availability_zones" "available" {
+#   state = "available"
+# }
+
+
